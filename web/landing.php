45112be9f5f14618262ca0c8e7fc7cc44d7da3d8
critterwilson <wil15090@byui.edu>
2019-05-02 20:35:29 -0600

    trying to fix php error

--- web/landing.php
+++ web/landing.php
@@ -5,18 +5,18 @@
 </head>
 <body>
 	<?php
-		$pages = scandir('ponder02');
+		// $pages = scandir('ponder02');
 		
-		echo '<div class="menu">';
+		// echo '<div class="menu">';
 
-		foreach ($pages as $page) {
-			if (!in_array($page, $notWanted){
-				$safeName = strtoupper(str_replace('.php', '', $page));
-				$link = 'ponder02'.$page;
-				echo '<a heref="'.$link.'">'.$safeName.'</a>';
-			}
-		}
-		echo '</div>';
+		// foreach ($pages as $page) {
+		// 	if (!in_array($page, $notWanted){
+		// 		$safeName = strtoupper(str_replace('.php', '', $page));
+		// 		$link = 'ponder02'.$page;
+		// 		echo '<a heref="'.$link.'">'.$safeName.'</a>';
+		// 	}
+		// }
+		// echo '</div>';
 	?>
 
 </body>
