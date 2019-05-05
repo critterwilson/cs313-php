8098a821a2c4b08b121d9ad9884a638698a7e1e9
critterwilson <wil15090@byui.edu>
2019-05-04 21:15:48 -0600

    adding bio info and picture, media query, hopefully php time on second page

---
+++ web/landing_1.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Christopher Wilson</title>
+	<link rel="stylesheet" type="text/css" href="landing.css">
+</head>
+<body>
+	<div class="topnav">
+		<a href="landing.html">About Me</a>
+		<a class="active" href="landing_1.html">Assignment List</a>
+	</div>
+	<div class="flex-container">
+		<div class="col">
+			<p><b>Name</b>: Christopher Wilson<br>
+				<b>Major</b>: Software Engineering<br>
+				<b>Hometown</b>: Denver, Colorado<br></p>
+			<p><b>Hobbies/Interests:</b>
+			<ul id="interests">
+				<li>Family Time</li>
+				<li>Guitar</li>
+				<li>Dogs</li>
+				<li>Drums/Percussion</li>
+				<li>Hockey (Colorado Avalanche)</li>
+				<li>Rugby</li>
+				<li>Slacklining</li>
+			</ul></p>
+		</div>
+
+		<div class="col2">
+			<img src="me.jpg" id="me" alt="My Wife and I">
+		</div>
+	</div>
+
+	<!-- <?php
+
+	?> -->
+
+</body>
+</html>
