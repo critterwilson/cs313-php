674f2755036ded79a09a243eabe16b800c2edaa7
critterwilson <wil15090@byui.edu>
2019-05-04 21:45:08 -0600

    Finishing touches

--- web/landing_1.php
+++ web/landing_1.php
@@ -5,8 +5,16 @@
 	<link rel="stylesheet" type="text/css" href="landing.css">
 </head>
 <body>
-
-<p><b>CS313 Assignments coming to a browser near you</b></p>
+	<div class="topnav">
+		<a href="landing.php">About Me</a>
+		<a class="active" href="landing_1.html">Assignment List</a>
+		<?php
+		date_default_timezone_set("MST");
+		echo "<p>".date("d/m/Y")."</p>";
+		?>
+	</div>
+	
+	<p><b>CS313 Assignments coming soon to a browser near you</b></p>
 
 </body>
 </html>
