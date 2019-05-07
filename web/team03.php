ac7c09fc41765360a0575afa8363acafbe3f8adf
critterwilson <wil15090@byui.edu>
2019-05-07 15:15:06 -0600

    updating team activity

--- web/team03.php
+++ web/team03.php
@@ -4,6 +4,10 @@
 	<title></title>
 </head>
 <body>
-Boobies
+
+	Weclcome <?php echo $_POST["name"];?><br>
+	Your email address is <?php echo $_POST["email"];?><br>
+	Your major is <?php echo $_POST["major"];?><br>
+	Comments: <?php echo $_POST["comments"];?><br>
 </body>
 </html>
