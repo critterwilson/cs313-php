7e397d1fe0e1e583c8c4b4b317f950c949a098f5
critterwilson <wil15090@byui.edu>
2019-05-07 13:10:10 -0600

    adding basic topnav to project1

---
+++ web/project1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="project1.css">
+	<title>Religion Teacher Sign-Up</title>
+</head>
+<body>
+	<div class="topnav"
+		<a class="active" href="project1.php">Home</a>
+		<a href="project1_1.php">Read Info</a>
+	</div>
+
+</body>
+</html>
