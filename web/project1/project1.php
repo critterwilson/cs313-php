848996af1756edf9758e2ba86b8f62dc5e732e88
critterwilson <wil15090@byui.edu>
2019-05-08 16:51:24 -0600

    adding database connection

--- web/project1/project1.php
+++ web/project1/project1.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="project1.css">
+	<script src="project1.js"></script>
 	<title>Religion Teacher Sign-Up</title>
 </head>
 <body>
@@ -34,15 +35,30 @@
 		  die();
 		}
 	?>
-	<!-- Database Queries -->
-	<?php
-		echo '<select>';
-		foreach ($db->query('SELECT name_first, name_last FROM professor') as $row)
-		{
-		  	echo '<option value='.$row['name_last'].'>'.$row['name_last'].','.$row['name_first'].'</option>';
-		}
-		echo '</select>'
-	?>
-	<div class="info to read">
+
+	<form id="teacherSignUp">
+	<!-- Database Query to fetch all professors-->
+		<?php
+			echo '<select id="professor">';
+			foreach ($db->query('SELECT name_first, name_last FROM professor') as $row)
+			{
+			  	echo '<option value='.$row['name_last'].'>'.$row['name_last'].', '.$row['name_first'].'</option>';
+			}
+			echo '</select>'
+		?>
+		<select id="numCourses" onchange="courseSignUp()">
+			<option value="0">0</option>
+			<option value="1">1</option>
+			<option value="2">2</option>
+			<option value="3">3</option>
+			<option value="4">4</option>
+			<option value="5">5</option>
+			<option value="6">6</option>
+		</select>
+
+
+	</form>
+
+
 </body>
 </html>
