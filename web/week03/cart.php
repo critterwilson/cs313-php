f9b9059bb4758b46072544ddf534c3acd4001a43
critterwilson <wil15090@byui.edu>
2019-05-09 16:10:34 -0600

    Adding clear cart function

--- web/week03/cart.php
+++ web/week03/cart.php
@@ -4,3 +4,17 @@
 	var_dump($_SESSION['cart']);
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form method="post" action="<?php session_unset(); ?>">
+	<input type="submit" value="Clear Cart">
+</form>
+
+</body>
+</html>
