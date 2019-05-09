f1707086b716cb6e4121350c93c4372d5e7b491a
critterwilson <wil15090@byui.edu>
2019-05-09 13:02:36 -0600

    adding javascript to offer sign up options

--- web/project1/project1.js
+++ web/project1/project1.js
@@ -2,7 +2,7 @@ function courseSignUp() {
     var h = document.getElementById("courseSignUp");
     for (var i = 0; i < 6; i++) {
         var courseNum = "courseSignUp_" + i;
-        if (i < document.getElementById("numCourses").value) {
+        if (i > document.getElementById("numCourses").value) {
             str = "Hello";
             h.insertAdjacentHTML("afterend",str);
         }
