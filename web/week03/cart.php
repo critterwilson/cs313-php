81d9146795e90e9b898a6e77df1635af2f707e5e
critterwilson <wil15090@byui.edu>
2019-05-11 19:01:12 -0600

    Adding Display andprices to items

--- web/week03/cart.php
+++ web/week03/cart.php
@@ -24,9 +24,32 @@
 
 	<ul id="fullCart">
 		<?php
-			foreach ($_SESSION['cart'] as $item) {
-				echo "<li>$item</li>";
+		$total = 0.00
+		foreach ($_SESSION['cart'] as $item) {
+			switch ($item) {
+				case 'item1':
+					$price = 14.95;
+					break;
+				case 'item2':
+					$price = 14.95;
+					break;
+				case 'item3':
+					$price = 14.95;
+					break;
+				case 'item4':
+					$price = 14.95;
+					break;
+				case 'item5':
+					$price = 14.95;
+					break;
+				case 'item6':
+					$price = 14.95;
+					break;
 			}
+
+			$total += $price;
+			echo "<li>$item, $price</li>";
+		}
 		?>
 	</ul>
 
