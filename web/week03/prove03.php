3bab5c6a1e4121af650f32035499754641296439
critterwilson <wil15090@byui.edu>
2019-05-11 21:36:28 -0600

    added css for home page

--- web/week03/prove03.php
+++ web/week03/prove03.php
@@ -20,6 +20,7 @@
 	<title>The Random Things Store</title>
 </head>
 <body>
+	<h1>The Random Things Store</h1>
 	<div class="topnav">
 		<a class="active" href="prove03.php">Browse</a>
 		<a href="cart.php">View Cart</a>
@@ -27,37 +28,49 @@
 
 	<div class="flex-container" id="itemList">
 		<div class="item">
-			Whosie<br>
+			<img src="spoon.jpg" alt="Whosie" class="itemPhoto">
+			<h4>Whosie</h4>
+			<h5>Price: $11.95</h5>
 			<form method="post" action="prove03.php">
 				<button type="submit" name="item" value="Whosie">Add to Cart</button>
 			</form>
 		</div>
 		<div class="item">
-			Whatsit<br>
+			<img src="shoes.jpg" alt="whatsit" class="itemPhoto">
+			<h4>Whatsit</h4>
+			<h5>Price: $12.95</h5>
 			<form method="post" action="prove03.php">
 				<button type="submit" name="item" value="Whatsit">Add to Cart</button>
 			</form>
 		</div>
 		<div class="item">
-			Thingy<br>
+			<img src="chipclip.jpg" alt="Thingy" class="itemPhoto">
+			<h4>Thingy</h4>
+			<h5>Price: $13.95</h5>
 			<form method="post" action="prove03.php">
 				<button type="submit" name="item" value="Thingy">Add to Cart</button>
 			</form>
 		</div>
 		<div class="item">
-			Mabob<br>
+			<img src="paperclip.jpg" alt="Mabob" class="itemPhoto">
+			<h4>Mabob</h4>
+			<h5>Price: $14.95</h5>
 			<form method="post" action="prove03.php">
 				<button type="submit" name="item" value="Mabob">Add to Cart</button>
 			</form>
 		</div>
 		<div class="item">
-			Gizmo<br>
+			<img src="comb.jpg" alt="Gizmo" class="itemPhoto">
+			<h4>Gizmo</h4>
+			<h5>Price: $15.95</h5>
 			<form method="post" action="prove03.php">
 				<button type="submit" name="item" value="Gizmo">Add to Cart</button>
 			</form>
 		</div>
 		<div class="item">
-			Snorfblat<br>
+			<img src="fork.jpg" alt="Snorfblat" class="itemPhoto">
+			<h4>Snorfblat</h4>
+			<h5>Price: $15.95</h5>
 			<form method="post" action="prove03.php">
 				<button type="submit" name="item" value="Snorfblat">Add to Cart</button>
 			</form>
