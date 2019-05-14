7ec080e1c7010ae50930f93ea70c6668c90241c4
critterwilson <wil15090@byui.edu>
2019-05-14 13:04:55 -0600

    trying to post multiple values

--- web/project1/insertSection.php
+++ web/project1/insertSection.php
@@ -6,11 +6,11 @@
 	for ($i = 0; $i < count($_POST['course_id']); $i++) { 
 		for ($j = 1; $j <= $_POST['amount'][$i]; $j++) { 
 			$course_id = $_POST['course_id'][$i];
-			$stmt = $db->prepare('INSERT INTO section(course_id, section_number, professor_id) VALUES (:course_id, :j, NULL) ON CONFLICT DO NOTHING;');
+			$stmt = $db->prepare('INSERT INTO section(course_id, section_number, professor_id) VALUES (:course_id, $j, NULL) ON CONFLICT DO NOTHING;');
 			$stmt->bindValue(':course_id', $course_id, PDO::PARAM_INT);
 			// $stmt->bindValue(":j", $j, PDO::PARAM_INT);
 
-			// echo $stmt.'<br>';
+			echo $stmt.'<br>';
 
 			// echo "INSERT INTO section (course_id, section_number, professor_id) ";
 			// echo "VALUES (".$_POST['course_id'][$i].", ".$j.", NULL) ";
