f26fb65a99ddcbec28dca7efc70df4b9170138a5
critterwilson <wil15090@byui.edu>
2019-05-14 11:08:24 -0600

    trying to post multiple values

--- web/project1/insertSection.php
+++ web/project1/insertSection.php
@@ -1,6 +1,32 @@
 <?php
-	var_dump($_POST['course_id']);
+	// Database Connection
+	try
+	{
+	  $dbUrl = getenv('DATABASE_URL');
 
-	var_dump($_POST['amount']);
+	  $dbOpts = parse_url($dbUrl);
+
+	  $dbHost = $dbOpts["host"];
+	  $dbPort = $dbOpts["port"];
+	  $dbUser = $dbOpts["user"];
+	  $dbPassword = $dbOpts["pass"];
+	  $dbName = ltrim($dbOpts["path"],'/');
+
+	  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+	  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+	catch (PDOException $ex)
+	{
+	  echo 'Error!: ' . $ex->getMessage();
+	  die();
+	}
+
+	// For every course in the list
+	for ($i = 0; $i < count($_POST['course_id']) $i++) { 
+		
+		$n = $_POST['amount'][$i];
+		echo $n;
+	}
 
 ?>
