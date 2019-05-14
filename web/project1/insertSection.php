51e2d5fd5ddadb84d19005f8325d4e41b1fee0e2
critterwilson <wil15090@byui.edu>
2019-05-14 12:06:41 -0600

    trying to post multiple values

--- web/project1/insertSection.php
+++ web/project1/insertSection.php
@@ -25,8 +25,8 @@
 	// For every course in the list
 	for ($i = 0; $i < count($_POST['course_id']); $i++) { 
 		for ($j = 1; $j <= $_POST['amount'][$i]; $j++) { 
-			echo "INSTER INTO section (course_id, section_number)";
-			echo "VALUES (".$_POST['course_id'][$i].", ".$j.")";
+			echo "INSTER INTO section (course_id, section_number) ";
+			echo "VALUES (".$_POST['course_id'][$i].", ".$j.") ";
 			echo "ON CONFLICT DO NOTHING;<br>";
 		}
 
