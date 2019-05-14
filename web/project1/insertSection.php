8d8d4bb343a7e6e0a4cc41d57fa405ef8906c214
critterwilson <wil15090@byui.edu>
2019-05-14 13:20:32 -0600

    finalizing the databse writing

--- web/project1/insertSection.php
+++ web/project1/insertSection.php
@@ -10,11 +10,10 @@
 			$stmt = $db->prepare('INSERT INTO section(course_id, section_number, professor_id) VALUES (:course_id,'.$j.', NULL) ON CONFLICT DO NOTHING;');
 			$stmt->bindValue(':course_id', $course_id, PDO::PARAM_INT);
 			$stmt->execute();
-
-			$new_page = "sectionCreation.php";
-			header("Location: $new_page");
-			die();
 		}
 	}
 
+	$new_page = "sectionCreation.php";
+	header("Location: $new_page");
+	die();
 ?>
