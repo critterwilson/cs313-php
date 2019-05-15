3ee7e21adae677eebf1ab620d67539bb579d2ce5
critterwilson <wil15090@byui.edu>
2019-05-15 13:40:19 -0600

    changes

--- web/project1/project1.js
+++ web/project1/project1.js
@@ -1,14 +1,3 @@
-function courseSignUp() {
-    var h = document.getElementById("courseSignUp");
-    for (i = 0; i < 6; i++) {
-        var courseNum = "courseSignUp_" + i;
-        if (i < document.getElementById("numCourses").value) {
-            str = "Hello";
-            h.insertAdjacentHTML("afterend",str);
-        }
-    }
-}
-
 function viewChange() {
 	var view = document.getElementById("viewType");
 	
