dc21915f020e1086d0aac0140240de4b088c933b
critterwilson <wil15090@byui.edu>
2019-05-15 13:44:05 -0600

    changes

--- web/project1/project1.js
+++ web/project1/project1.js
@@ -4,7 +4,7 @@ function viewChange() {
 	var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("viewInfo").innerHTML = this.responseText;
+        console.log(this.responseText);
       }
     };
     xmlhttp.open("GET", "setView.php?q=" + view, true);
