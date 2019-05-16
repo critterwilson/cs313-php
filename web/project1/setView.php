6f40493e1a678d88b1a503d72df278e335a9ab25
critterwilson <wil15090@byui.edu>
2019-05-16 09:00:31 -0600

    trying to get ajax to work

--- web/project1/setView.php
+++ web/project1/setView.php
@@ -1,7 +1,15 @@
 <?php
+	require('databaseConnection.php');
+	$db = get_db();
+
 	switch($_REQUEST["q"]) {
 		case 1:
-			
+			echo '<select id="professor">';
+			foreach ($db->query('SELECT name_first, name_last FROM professor') as $row)
+			{
+			  	echo '<option value='.$row['name_last'].'>'.$row['name_last'].', '.$row['name_first'].'</option>';
+			}
+			echo '</select>';	
 			break;
 		case 2:
 			echo "Case 1";
