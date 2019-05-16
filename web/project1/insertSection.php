b6094bc05784eff04437588a52a5db42e7005264
critterwilson <wil15090@byui.edu>
2019-05-16 10:20:14 -0600

    trying to get ajax to work

--- web/project1/insertSection.php
+++ web/project1/insertSection.php
@@ -6,6 +6,7 @@
 	for ($i = 0; $i < count($_POST['course_id']); $i++) {
 		$course_id = $_POST['course_id'][$i];
 		$amount = $_POST['amount'][$i];
+
 		for ($j = 1; $j <= $_POST['amount'][$i]; $j++) { 
 			// INSERT INTO section(course_id, section_number, professor_id) VALUES course_id, index, NULL) ON CONFLICT DO NOTHING;
 			$stmt = $db->prepare('INSERT INTO section(course_id, section_number, professor_id) VALUES (:course_id,'.$j.', NULL) ON CONFLICT DO NOTHING;');
@@ -13,10 +14,12 @@
 			$stmt->execute();
 		}
 		
-		// $stmt = $db->prepare('SELECT * FROM section WHERE section_number > :amount AND course_id = :course_id;');
-		// $stmt->bindValue(':course_id', $course_id, PDO::PARAM_INT);
-		// $stmt->bindValue(':amount',  $amount, PDO::PARAM_INT);
-		// echo $stmt;
+		//SELECT * FROM section WHERE course_id = :course_id AND section_number > :amount;
+
+		$stmt = $db->prepare('SELECT * FROM section WHERE course_id = :course_id AND section_number > :amount;');
+		$stmt->bindValue(':course_id', $course_id, PDO::PARAM_INT);
+		$stmt->bindValue(':amount',  $amount, PDO::PARAM_INT);
+		echo $stmt;
 	}
 
  	$new_page = "sectionCreation.php";
