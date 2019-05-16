b005038e06da488b0a56d23719641b3d2d7a7958
critterwilson <wil15090@byui.edu>
2019-05-16 09:13:10 -0600

    trying to get ajax to work

--- web/project1/project1.js
+++ web/project1/project1.js
@@ -1,11 +1,12 @@
-window.onload = function viewChange() {
+function viewChange() {
 	var view = document.getElementById("viewType").value;
 	
+	document.getElementById("viewInfo").innerHTML = "Hello";
 	var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         console.log(this.responseText);
-        document.getElementById("viewInfo").innerHTML = this.responseText;
+        //document.getElementById("viewInfo").innerHTML = this.responseText;
       }
     };
     xmlhttp.open("GET", "setView.php?q=" + view, true);
