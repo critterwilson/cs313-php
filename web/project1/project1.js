17e125a9fe5edafe6479e17996296a3581eb862c
critterwilson <wil15090@byui.edu>
2019-05-16 09:31:52 -0600

    trying to get ajax to work

--- web/project1/project1.js
+++ web/project1/project1.js
@@ -1,8 +1,8 @@
 function viewChange() {
 	var view = document.getElementById("viewType").value;
 	
-	document.getElementById("viewInfo").innerHTML = "Hello";
-	
+	document.getElementById('viewInfo').innerHTML = "Hello";
+
 	var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
