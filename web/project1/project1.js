c05c33fba6573c6c6360bd8f2bb739faf67dbd72
critterwilson <wil15090@byui.edu>
2019-05-20 12:14:54 -0600

    trying to get innerhtml to work

--- web/project1/project1.js
+++ web/project1/project1.js
@@ -1,16 +1,14 @@
 function viewChange() {
-  document.addEventListener('DOMContentLoaded', function () {
-    var view = document.getElementById("viewType").value;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
-        document.getElementById("output").innerHTML = this.responseText;
-      }
-    };
-    xmlhttp.open("GET", "setView.php?q=" + view, true);
-    xmlhttp.send();
- });
+  var view = document.getElementById("viewType").value;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(this.responseText);
+      document.getElementById("output").innerHTML = this.responseText;
+    }
+  };
+  xmlhttp.open("GET", "setView.php?q=" + view, true);
+  xmlhttp.send();
 }
 
 
