ba046ab0e3eab04a8d44719acd3d1dc44a0213ff
critterwilson <wil15090@byui.edu>
2019-05-20 12:10:43 -0600

    trying to get innerhtml to work

--- web/project1/project1.js
+++ web/project1/project1.js
@@ -1,28 +1,16 @@
 function viewChange() {
-  if( document.readyState !== 'loading' ) {
+  document.addEventListener('DOMContentLoaded', function () {
     var view = document.getElementById("viewType").value;
     var xmlhttp = new XMLHttpRequest();
-      xmlhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          console.log(this.responseText);
-          document.getElementById("viewInfo").innerHTML = this.responseText;
-        }
-      };
-      xmlhttp.open("GET", "setView.php?q=" + view, true);
-      xmlhttp.send();
-} else {
-    document.addEventListener('DOMContentLoaded', function () {
-      var view = document.getElementById("viewType").value;
-      var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
-            document.getElementById("viewInfo").innerHTML = this.responseText;
-          }
-        };
-        xmlhttp.open("GET", "setView.php?q=" + view, true);
-        xmlhttp.send();
- }
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseText);
+        document.getElementById("viewInfo").innerHTML = this.responseText;
+      }
+    };
+    xmlhttp.open("GET", "setView.php?q=" + view, true);
+    xmlhttp.send();
+ });
 }
 
 
