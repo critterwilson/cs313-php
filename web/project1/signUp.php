0c7061fdf122f85601ff042ddffc015d74901555
critterwilson <wil15090@byui.edu>
2019-05-20 13:46:09 -0600

    starting on the professor sign up section

--- web/project1/signUp.php
+++ web/project1/signUp.php
@@ -6,7 +6,7 @@
 		echo '<select id="courseSelect_'.$i.'">';
 		// psql: SELECT * FROM section
 		//       JOIN course ON section.course_id = course.id WHERE taken = false;
-		foreach ($db->query('SELECT * FROM section JOIN course ON section.course_id = course.id WHERE taken = false ORDER BY section_number ASC;') as $row)
+		foreach ($db->query('SELECT * FROM section JOIN course ON section.course_id = course.id WHERE taken = false ORDER BY postfix ASC, section_number ASC;') as $row)
 		{
 			// <option value='2757'>RELC275:7 Teachings of the Book of Mormon</option>
 		  	echo '<option value="'.$row['postfix'].$row['section_number'].'"">'.$row['prefix'].$row['postfix'].':'.$row['section_number'].' '.$row['name'].'</option>';
