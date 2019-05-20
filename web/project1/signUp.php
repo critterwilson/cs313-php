5422a2ec94f13490db748ccd396900c83ff3f392
critterwilson <wil15090@byui.edu>
2019-05-20 13:36:53 -0600

    starting on the professor sign up section

--- web/project1/signUp.php
+++ web/project1/signUp.php
@@ -9,7 +9,7 @@
 		foreach ($db->query('SELECT * FROM section JOIN course ON section.course_id = course.id;') as $row)
 		{
 			// <option value='2757'>RELC275:7 Teachings of the Book of Mormon</option>
-		  	echo '<option value='$row['postfix'].$row['section_number'].'>'.$row['prefix'].$row['postfix'].':'.$row['section_number'].' '.$row['name'].'</option>';
+		  	echo '<option value='.$row['postfix'].$row['section_number'].'>'.$row['prefix'].$row['postfix'].':'.$row['section_number'].' '.$row['name'].'</option>';
 		}	
 		echo '</select>';
 	}
