9e8cdb87667aa6350d34008f7d2a4efc0b91f646
critterwilson <wil15090@byui.edu>
2019-05-20 12:58:02 -0600

    getting readInfo teacher setting to work

--- web/project1/setView.php
+++ web/project1/setView.php
@@ -5,7 +5,7 @@
 	switch($_REQUEST["q"]) {
 		case 1:
 			// SELECT * FROM professor ORDER BY name_last asc, name_first asc;
-			foreach ($db->query('SELECT * FROM professor ORDER BY name_last ASC, name_first ASC;') as $row)
+			foreach ($db->query('SELECT name_last, name_first FROM professor ORDER BY name_last ASC, name_first ASC;') as $row)
 			{
 			  	echo $row['name_last'].', '.$row['name_last']
 			}	
