a7a5218af74432a72935166a0acc10feac25df27
critterwilson <wil15090@byui.edu>
2019-05-21 11:32:21 -0600

    trying out a new signUp format

--- web/project1/project1.php
+++ web/project1/project1.php
@@ -31,7 +31,7 @@
 			echo '</select>'
 		?>
 
-		<select id="numCourses" onchange="courseSignUp()">
+		<select id="numCourses" onchange="courseSignUp_Class()">
 			<option value="0">0</option>
 			<option value="1">1</option>
 			<option value="2">2</option>
