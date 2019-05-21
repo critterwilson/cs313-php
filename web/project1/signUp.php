1dd9e4af4a45ab1af251aafc1fdb34878f3123cb
critterwilson <wil15090@byui.edu>
2019-05-21 12:31:01 -0600

    trying out a new signUp format

--- web/project1/signUp.php
+++ web/project1/signUp.php
@@ -7,7 +7,7 @@
 			echo '<div id="courseSelect_'.$i.'">';
 			echo '<select onchange="courseSignUp_Section()">';
 			// psql: SELECT * FROM class ORDER BY postfix ASC, prefix ASC;
-			foreach ($db->query('SELECT * FROM course ORDER BY postfix ASC, prefix ASC;') as $row)
+			foreach ($db->query('SELECT course.prefix, course.postfix, course.name, MIN(section.section_number) FROM course JOIN section ON course.id = section.course_id WHERE section.taken = false GROUP BY course.prefix, course.postfix, course.name ORDER BY course.postfix ASC;') as $row)
 			{
 				// <option value='RELC275'>RELC275 Teachings of the Book of Mormon</option>
 			  	echo '<option value="'.$row['prefix'].$row['postfix'].'">'.$row['prefix'].$row['postfix'].' '.$row['name'].'</option>';
@@ -17,3 +17,9 @@
 		}
 	}
 ?>
+
+SELECT course.prefix, course.postfix, course.name, MIN(section.section_number) 
+FROM course JOIN section ON course.id = section.course_id 
+WHERE section.taken = false 
+GROUP BY course.prefix, course.postfix, course.name
+ORDER BY course.postfix ASC;
