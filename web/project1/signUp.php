0e7395d7f59945957348ec7924f3ec83fee8a7b4
critterwilson <wil15090@byui.edu>
2019-05-28 12:59:14 -0600

    trying to test out using two select items on one line

--- web/project1/signUp.php
+++ web/project1/signUp.php
@@ -6,7 +6,7 @@
 	if (isset($_REQUEST["q"])) {
 		// for the number of classes desired
 		for ($i = 0; $i < $_REQUEST["q"]; $i++) { 
-			echo '<div id="signUp_'.$i.'">';
+			echo '<div class="signUp" id="signUp_'.$i.'">';
 			echo '<select id="courseSelect_'.$i.'" onchange="sectionSignUp('.$i.')">';
 			
 			# psql: SELECT course.prefix, course.postfix, course.name, MIN(section.section_number) 
