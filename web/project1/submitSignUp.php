71d2ecc0a0a00d4cff8709547413088af37bf507
critterwilson <wil15090@byui.edu>
2019-05-29 13:19:22 -0600

    trying to figure out submit course

--- web/project1/submitSignUp.php
+++ web/project1/submitSignUp.php
@@ -5,4 +5,9 @@
 	echo $_REQUEST["professor"];
 	echo $_REQUEST["numCourses"];
 	var_dump($_REQUEST);
+
+	for ($i=0; $i < $_REQUEST["numCourses"]; $i++) { 
+		$professorID = $db->query('SELECT id FROM professor WHERE name_last = \'Allred\';');
+		echo $professorID;
+	}
 ?>
