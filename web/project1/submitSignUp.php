2997c5c5fdfedf5cc6da4937d73a37e4ff3cf5b5
critterwilson <wil15090@byui.edu>
2019-05-29 13:58:50 -0600

    trying to figure out submit course

--- web/project1/submitSignUp.php
+++ web/project1/submitSignUp.php
@@ -7,13 +7,14 @@
 	var_dump($_REQUEST);
 	echo '<br><br><br>';
 
+	
 	for ($i=0; $i < $_REQUEST["numCourses"]; $i++) { 
-		$stmt = $db->prepare('SELECT id FROM professor WHERE name_last=:name');
-		$stmt->bindValue(':name', 'Allred', PDO::PARAM_STR);
-		$stmt->execute();
-		$row = $stmt->fetchAll(PDO::FETCH_ASSOC);
+		#psql: UPDATE section SET professor_id = 
+		#		 (SELECT id FROM professor WHERE name_last = 'Allred' AND name_first = 'Philip'), 
+		#		 taken = true 
+		#	   WHERE course_id = 
+		#		 (SELECT id FROM course WHERE postfix = 325) AND section_number = 1; 
 
-		var_dump($row);
 
 	}
 ?>
