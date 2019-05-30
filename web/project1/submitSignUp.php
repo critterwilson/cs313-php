cd2f93aeaa968fd9f2e31029181be3b58c561ed3
critterwilson <wil15090@byui.edu>
2019-05-30 11:35:17 -0600

    getting professor string to explode correctly

--- web/project1/submitSignUp.php
+++ web/project1/submitSignUp.php
@@ -2,7 +2,7 @@
 	require('databaseConnection.php');
 	$db = get_db();
 
-	var_dump(explode(',', $_REQUEST["professor"]));
+	var_dump($_REQUEST["professor"]);
 	echo $_REQUEST["numCourses"];
 	var_dump($_REQUEST);
 	echo '<br><br><br>';
@@ -12,8 +12,7 @@
 		#psql: UPDATE section SET professor_id = 
 		#		 (SELECT id FROM professor WHERE name_last = 'Allred' AND name_first = 'Philip'), 
 		#		 taken = true 
-		#	   WHERE course_id = 
-		#		 (SELECT id FROM course WHERE postfix = 325) AND section_number = 1; 
+		#	   WHERE course_id = course.id AND section_number = 1; 
 
 
 	}
