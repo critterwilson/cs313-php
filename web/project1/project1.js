537ed29ce1d59ce82ec1de27f1465be07073e5b0
critterwilson <wil15090@byui.edu>
2019-05-30 11:51:43 -0600

    getting professor string to explode correctly

--- web/project1/project1.js
+++ web/project1/project1.js
@@ -35,7 +35,7 @@ function courseSignUp() {
     }
   };
   xmlhttp.open("POST", "signUp.php", true);
-  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xmlhttp.send("q=" + numSignUp);
 }
 
