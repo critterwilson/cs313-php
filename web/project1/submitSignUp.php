3801abf560529a73b8144e2921a99931341cf7e5
critterwilson <wil15090@byui.edu>
2019-05-30 12:05:51 -0600

    getting professor string to explode correctly

--- web/project1/submitSignUp.php
+++ web/project1/submitSignUp.php
@@ -4,14 +4,15 @@
 
 	var_dump($_POST);
 	echo '<br><br><br>';
+	$professor_id = $_POST['professor'];
 
-	
 	for ($i=0; $i < $_POST["numCourses"]; $i++) { 
-		#psql: UPDATE section SET professor_id = 
-		#		 (SELECT id FROM professor WHERE name_last = 'Allred' AND name_first = 'Philip'), 
-		#		 taken = true 
-		#	   WHERE course_id = course.id AND section_number = 1; 
+		$course_id = $_POST['courseSelect_'.$i];
+		$section_number = $_POST['sectionSelect_'.$i];
 
+		#psql: UPDATE section SET professor_id = $professor_id, taken = true 
+		#	   WHERE course_id = $course.id AND section_number = $section_number; 
+		echo 'UPDATE section SET professor_id = '.$professor_id.', taken = true WHERE course_id ='.$course_id.' AND section_number = '.$section_number.';';
 
 	}
 ?>
