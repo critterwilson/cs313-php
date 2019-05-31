0a07626b03955e7f47963ef0e15855eb7a50ebf7
critterwilson <wil15090@byui.edu>
2019-05-31 12:56:50 -0600

    adding a remove button to the table

--- web/project1/remove.php
+++ web/project1/remove.php
@@ -8,4 +8,9 @@
 
 	echo "$course_id, $professor_id, $section_number";
 
+	$stmt = $db->prepare('UPDATE section SET professor_id = "null", taken = false WHERE course_id ='.$course_id.' AND section_number = '.$section_number.' AND professor_id = '.$professor_id';');
+
+	// binding course id might not be necessary because it isn't entered by the user
+	$stmt->execute();
+
 ?>
