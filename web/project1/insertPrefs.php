894157d9ec605da9f7b1b368123eed903ebce433
critterwilson <wil15090@byui.edu>
2019-06-07 13:12:41 -0600

    adding a form to assign professor preferences

--- web/project1/insertPrefs.php
+++ web/project1/insertPrefs.php
@@ -1,4 +1,8 @@
 <?php
+	// make sure we connect to the database
+	require('databaseConnection.php');
+	$db = get_db();
+
 	var_dump($_POST);
 	$professor_id = $_POST['professor'];
 	$office = $_POST['office'];
@@ -6,7 +10,11 @@
 	$seating = $_POST['seating'];
 	$mac = $_POST['mac'];
 
+	$stmt = $db->prepare("INSERT INTO professor_prefs (professor_id, office, instrument, mac, seating) VALUES ($professor_id, $office, $instrument, $mac, $seating;");
+	$stmt->execute();
+
 	foreach ($_POST['time'] as $time) {
-		echo $time;
+		$stmt = $db->prepare("UPDATE professor_prefs SET $time = 'true' WHERE professor_id = $professor_id;")
+		$stmt->execute();
 	}
 ?>
