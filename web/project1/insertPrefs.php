867e792b96c584f39f1eaa65473a69d845f21b01
critterwilson <wil15090@byui.edu>
2019-06-10 13:32:28 -0600

    adding a form to assign professor preferences

--- web/project1/insertPrefs.php
+++ web/project1/insertPrefs.php
@@ -5,9 +5,9 @@
 
 	# INSERT INTO professor_prefs (professor_id, office, instrument, mac, seating) VALUES (6, 'TAY', 2, false, 2) ON CONFLICT (professor_id) DO UPDATE SET office = 'TAY', instrument = 2, mac = false, seating = 2;
 	// Insert the data into the table, update if professor_id is already recorded
-	$insert = "INSERT INTO professor_prefs (professor_id, office, instrument, mac, seating) VALUES ($professor_id, '$office', $instrument, $mac, $seating) ON CONFLICT (professor_id) DO UPDATE SET office = '$office', instrument = $instrument, mac = $mac, seating = $seating;";
-	$stmt = $db->prepare($insert);
-	$stmt->execute();
+	// $insert = "INSERT INTO professor_prefs (professor_id, office, instrument, mac, seating) VALUES ($professor_id, '$office', $instrument, $mac, $seating) ON CONFLICT (professor_id) DO UPDATE SET office = '$office', instrument = $instrument, mac = $mac, seating = $seating;";
+	// $stmt = $db->prepare($insert);
+	// $stmt->execute();
 
 	// Set everything to false
 	// $reset = "UPDATE professor_prefs SET time0745 = false, time0900 = false, time1015 = false, time1130 = false, time1245 = false, time1400 = false, time1515 = false, time1630 = false WHERE professor_id = $professor_id;";
