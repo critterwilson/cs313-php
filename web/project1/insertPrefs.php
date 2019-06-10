24111c7631e87917d465cdd63abac15f954402a1
critterwilson <wil15090@byui.edu>
2019-06-10 13:31:54 -0600

    adding a form to assign professor preferences

--- web/project1/insertPrefs.php
+++ web/project1/insertPrefs.php
@@ -10,9 +10,9 @@
 	$stmt->execute();
 
 	// Set everything to false
-	$reset = "UPDATE professor_prefs SET time0745 = false, time0900 = false, time1015 = false, time1130 = false, time1245 = false, time1400 = false, time1515 = false, time1630 = false WHERE professor_id = $professor_id;";
-	$stmt = $db->prepare($reset);
-	$stmt->execute();
+	// $reset = "UPDATE professor_prefs SET time0745 = false, time0900 = false, time1015 = false, time1130 = false, time1245 = false, time1400 = false, time1515 = false, time1630 = false WHERE professor_id = $professor_id;";
+	// $stmt = $db->prepare($reset);
+	// $stmt->execute();
 
 
  	# UPDATE professor_prefs SET time0900 = true;
