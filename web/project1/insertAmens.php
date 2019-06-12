ede8bf19a4e936e0516995529a05d5fcc6b32c46
critterwilson <wil15090@byui.edu>
2019-06-12 13:28:49 -0600

    adding room setup

--- web/project1/insertAmens.php
+++ web/project1/insertAmens.php
@@ -15,7 +15,7 @@
 	$secondary_owner = $_POST['secondary_owner'];
 	# INSERT INTO room (building, room_number, piano, keyboard, mac, seating, capacity, primary_ownder, secondary_owner) VALUES (...) ON CONFLICT (unique_room) DO UPDATE SET piano = ..., keyboard = ..., mac = ..., seating = 2, capacity = ..., primary_owner = ..., secondary_owner = ...;
 
-	$insert = "INSERT INTO room (building, room_number, piano, keyboard, mac, seating, capacity, primary_owner, secondary_owner) VALUES ('$building', $room_number, $piano, $keyboard, $mac, $seating, $capacity, $primary_owner, $secondary_owner) ON CONFLICT (unique_room) DO UPDATE SET piano = $piano, keyboard = $keyboard, mac = $mac, seating = $seating, capacity = $capacity, primary_owner = $primary_owner, secondary_owner = $secondary_owner;";
+	$insert = "INSERT INTO room (building, room_number, piano, keyboard, mac, seating, capacity, primary_owner, secondary_owner) VALUES ('$building', $room_number, $piano, $keyboard, $mac, $seating, $capacity, '$primary_owner', '$secondary_owner') ON CONFLICT (building, room_number) DO UPDATE SET piano = $piano, keyboard = $keyboard, mac = $mac, seating = $seating, capacity = $capacity, primary_owner = '$primary_owner', secondary_owner = '$secondary_owner';";
 	echo $insert;
 
 ?>
