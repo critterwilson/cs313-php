4c3969daa64b2d618a2659a9544d9b85ff93c2b8
critterwilson <wil15090@byui.edu>
2019-06-14 08:06:40 -0600

    adding professor sign up

--- web/project1/insertProfessor.php
+++ web/project1/insertProfessor.php
@@ -7,8 +7,8 @@
 	$name_first = htmlspecialchars($_POST['name_first']);
 
 	#psql: INSERT INTO professor (name_first, name_last, adjunct) VALUES ('Christopher', 'Wilson', false) ON CONFLICT (name_first, name_last) DO UPDATE set adjunct = true;
+	$query = "INSERT INTO professor (name_first, name_last, adjunct) VALUES ('$name_last','$name_first',$_POST['adjunct']) ON CONFLICT (name_first, name_last) DO UPDATE set adjunct = $_POST['adjunct'];";
+	$stmt = $db->prepare($query);
 
-	$stmt = $db->prepare("INSERT INTO professor (name_first, name_last, adjunct) VALUES ('$name_last','$name_first',$_POST['adjunct']) ON CONFLICT (name_first, name_last) DO UPDATE set adjunct = $_POST['adjunct'];");
-	
 	$stmt->execute();
 ?>
