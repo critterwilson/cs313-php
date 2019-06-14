2180a1be128c6d176a6a1064c3c68fbf90a49c4b
critterwilson <wil15090@byui.edu>
2019-06-14 10:34:06 -0600

    adding course creation

--- web/project1/remove.php
+++ web/project1/remove.php
@@ -6,7 +6,7 @@
 		// remove a professor
 		if (isset($_POST['tID'])) {
 			$professor_id = $_POST['tID'];
-			$stmt = $db->prepare("UPDATE section SET professor_id = null WHERE professor_id = professor_id;");
+			$stmt = $db->prepare("UPDATE section SET professor_id = null WHERE professor_id = $professor_id;");
 			$stmt->exectute();
 
 			$stmt = $db->prepare('DELETE FROM professor WHERE id = '.$professor_id.';');
