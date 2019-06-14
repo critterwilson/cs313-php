9e62e4164fe8a198d00e3f70173f1d283a7fe5a8
critterwilson <wil15090@byui.edu>
2019-06-14 08:05:17 -0600

    adding professor sign up

--- web/project1/professorCreation.php
+++ web/project1/professorCreation.php
@@ -17,17 +17,17 @@
 	<p class="instructions"><b>Instructions:</b> </p>
 
 	<form id="teacherSetUp" action="insertProfessor.php" method="POST">
-		<h4>Which building is your office in?</h4>
+		<h4>First name:</h4>
 		<input type="text" name="name_first" size="20" pattern="^[a-zA-Z]+(([',.-][a-zA-Z ])?[a-zA-Z]*)*$" required>
 
-		<h4>Which instrument do you prefer?</h4>
+		<h4>Last name:</h4>
 		<input type="text" name="name_last" size="20" pattern="^[a-zA-Z]+(([',.-][a-zA-Z ])?[a-zA-Z]*)*$" required>
 
 		<h4>Are you and adjunct professor?</h4>
 		<select name="adjunct" requried>
 			<option value="false">No</option>
 			<option value="true">Yes</option>
-		</select><br>
+		</select><br><br>
 
 		<button type="submit" class="button">Submit</button>
 	</form>
