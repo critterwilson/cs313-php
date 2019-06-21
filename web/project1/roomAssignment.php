79ada288e0d3ef8cfc89f8a4b30153ce3c4ecf92
critterwilson <wil15090@byui.edu>
2019-06-21 13:07:18 -0600

    starting our quest for constraint satisfaction

--- web/project1/roomAssignment.php
+++ web/project1/roomAssignment.php
@@ -2,10 +2,28 @@
 	require('databaseConnection.php');
 	$db = get_db();
 
-	function scheduleMatch($prof_id, $room_id) {
+	function scheduleMatch($db, $prof_id, $room_id, $day) {
 		foreach($db->query("SELECT time0745, time0900, time1015, time1130, time1245, time1400, time1515, time1630 FROM professor_prefs WHERE professor_id = $prof_id;") as $prof_time)
 		{
-			echo $prof_time['time0745'];
+			foreach($db->query("SELECT time0745, time0900, time1015, time1130, time1245, time1400, time1515, time1630 FROM schedule_$day WHERE room_id = $room_id;") as $room_time)
+			{
+				if ($prof_time['time0745'] == true && $room_time['time0745'] == "null")
+					echo "7:45 true ";
+				if ($prof_time['time0900'] == true && $room_time['time0900'] == "null")
+					echo "9:00 true ";
+				if ($prof_time['time1015'] == true && $room_time['time1015'] == "null")
+					echo "10:15 true ";
+				if ($prof_time['time1130'] == true && $room_time['time1130'] == "null")
+					echo "11:30 true ";
+				if ($prof_time['time1245'] == true && $room_time['time1245'] == "null")
+					echo "12:45 true ";
+				if ($prof_time['time1400'] == true && $room_time['time1400'] == "null")
+					echo "2:00 true ";
+				if ($prof_time['time1515'] == true && $room_time['time1515'] == "null")
+					echo "3:15 true ";
+				if ($prof_time['time1630'] == true && $room_time['time1630'] == "null")
+					echo "4:30 true<br>";
+			}
 		}
 
 		return;
@@ -19,7 +37,7 @@
 	{
 		foreach($db->query("SELECT id FROM room;") as $room)
 		{
-			scheduleMatch($prof['professor_id'], $room['id']);
+			scheduleMatch($db, $prof['professor_id'], $room['id'], 'mon');
 		}
 	}
 	echo 'done';
