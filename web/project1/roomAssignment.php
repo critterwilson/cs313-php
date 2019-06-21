80cee263d7204147c4f18f74a4100baee3ac4a82
critterwilson <wil15090@byui.edu>
2019-06-21 12:47:27 -0600

    starting our quest for constraint satisfaction

--- web/project1/roomAssignment.php
+++ web/project1/roomAssignment.php
@@ -4,10 +4,10 @@
 
 	function scheduleMatch($prof_id, $room_id) {
 		echo $prof_id;
-//		foreach($db->query("SELECT time0745, time0900, time1015, time1130, time1245, time1400, time1515, time1630 FROM professor_prefs WHERE professor_id = $prof_id;") as $prof_time)
-		{
-			var_dump($prof_time);
-		}
+		// foreach($db->query("SELECT time0745, time0900, time1015, time1130, time1245, time1400, time1515, time1630 FROM professor_prefs WHERE professor_id = $prof_id;") as $prof_time)
+		// {
+		// 	var_dump($prof_time);
+		// }
 
 		return;
 	}
