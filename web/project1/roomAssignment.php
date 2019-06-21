bc20ca89a77e63d0baaeec52223801e2269e370d
critterwilson <wil15090@byui.edu>
2019-06-21 12:08:18 -0600

    starting our quest for constraint satisfaction

--- web/project1/roomAssignment.php
+++ web/project1/roomAssignment.php
@@ -2,13 +2,13 @@
 	require('databaseConnection.php');
 	$db = get_db();
 
-	function getScheduleMatch(prof_id, room_id) {
+	// function getScheduleMatch(prof_id, room_id) {
 
-	}
+	// }
 
-	function getPrefMatch(prof_id, room_id) {
+	// function getPrefMatch(prof_id, room_id) {
 
-	}
+	// }
 	
 	foreach($db->query("SELECT professor_id from section WHERE professor_id != null;") as $prof)
 	{
